6add79dc74d065d5575caa086c2a0092bb17cad4
StephanieVA <66794568+StephanieVA@users.noreply.github.com>
2026-07-23 23:40:10 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,23 @@
 import { Routes } from '@angular/router';
-
 import { LoginComponent } from './pages/login/login.component';
-
 import { CuestionarioComponent } from './pages/cuestionario/cuestionario.component';
-
 import { ReportesComponent } from './pages/reportes/reportes.component';
-
+import { EncuestaComponent } from './pages/encuesta/encuesta.component';
 export const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
   },
-
   {
     path: 'cuestionario',
     component: CuestionarioComponent,
   },
-
   {
     path: 'reportes',
     component: ReportesComponent,
+  },
+  {
+    path: 'encuesta',
+    component: EncuestaComponent
   },
 ];
