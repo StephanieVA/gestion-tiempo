17ad1106f568ef0161f06fe5fa705f7d9ea30b8d
StephanieVA <66794568+StephanieVA@users.noreply.github.com>
2026-07-23 23:11:48 -0500

    Update encuesta.service.ts

--- src/app/services/encuesta.service.ts
+++ src/app/services/encuesta.service.ts
@@ -1,1 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+@Injectable({
+  providedIn: 'root'
+})
+export class EncuestaService {
+
+  private api = 'http://backend-gestion-production-b3b8.up.railway.app/api/encuesta';
+
+  constructor(private http: HttpClient) {}
+
+  validarDni(dni: string): Observable<any> {
+    return this.http.get(`${this.api}/validar-dni/${dni}`);
+  }
+
+  obtenerCursos(semestre: string): Observable<any> {
+    return this.http.get(`${this.api}/cursos/${semestre}`);
+  }
+}
